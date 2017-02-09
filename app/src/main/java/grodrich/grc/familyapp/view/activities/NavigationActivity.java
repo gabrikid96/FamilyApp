c4f0addb6cc1e6ae6b955c01d8a1da4460c52b41
gabrikid <gabrikid96@gmail.com>
2017-02-09 12:02:19 +0100

    Prova branch

--- app/src/main/java/grodrich/grc/familyapp/view/activities/NavigationActivity.java
+++ app/src/main/java/grodrich/grc/familyapp/view/activities/NavigationActivity.java
@@ -134,6 +134,8 @@ public class NavigationActivity extends OptionsActivity {
                 Notification notification = dataSnapshot.getValue(Notification.class);
                 if (notification != null) {
                     //Snackbar.make(navigationView, notification.getText(), Snackbar.LENGTH_SHORT).show();
+
+                    
                     Toast.makeText(NavigationActivity.this,notification.getText(),Toast.LENGTH_SHORT).show();
                 }
             }
