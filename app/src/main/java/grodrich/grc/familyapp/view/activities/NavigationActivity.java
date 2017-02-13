4b7c32cd75ce4a43d5b3b0621f05f7f0e2ae74e4
gabri <gabrikid96@gmail.com>
2017-02-13 23:36:18 +0100

    Notifications en curso

--- app/src/main/java/grodrich/grc/familyapp/view/activities/NavigationActivity.java
+++ app/src/main/java/grodrich/grc/familyapp/view/activities/NavigationActivity.java
@@ -9,6 +9,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -29,6 +31,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import grodrich.grc.familyapp.R;
@@ -45,7 +48,9 @@ public class NavigationActivity extends OptionsActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private Menu toolbarMenu;
     ActionBarDrawerToggle mDrawerToggle;
+    ArrayList<Notification> notifications = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,6 @@ public class NavigationActivity extends OptionsActivity {
         setContentView(R.layout.navigation_activity);
         getViewsByXML();
         listeners();
-
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -132,10 +136,16 @@ public class NavigationActivity extends OptionsActivity {
         ctrl.getNotificacionReference().child(ctrl.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Notification notification = dataSnapshot.getValue(Notification.class);
-                if (notification != null) {
-                    Snackbar.make(navigationView, notification.getText(), Snackbar.LENGTH_SHORT).show();
-                    //Toast.makeText(NavigationActivity.this,notification.getText(),Toast.LENGTH_SHORT).show();
+                Notification notification;
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    notification = postSnapshot.getValue(Notification.class);
+                    if (!notifications.contains(notification)) {
+                        notifications.add(postSnapshot.getValue(Notification.class));
+                    }
+                    // TODO : que solo detecte cambios
+                }
+                if (notifications.get(0) != null) {
+                    Snackbar.make(navigationView, notifications.get(0).getText(), Snackbar.LENGTH_SHORT).show();
                 }
             }
 
@@ -205,6 +215,8 @@ public class NavigationActivity extends OptionsActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.toolbar_items, menu);
+        toolbarMenu = menu;
+        toolbarListener();
         return true;
     }
 
@@ -212,11 +224,18 @@ public class NavigationActivity extends OptionsActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.notifications:
-
                 break;
         }
         return true;
     }
+
+    private void toolbarListener(){
+        TextView counter = (TextView) toolbarMenu.findItem(R.id.notifications).getActionView().findViewById(R.id.notification_count);
+        counter.setText(String.valueOf(notifications.size()));
+        //TODO : listener
+    }
+
+
 
 
 
